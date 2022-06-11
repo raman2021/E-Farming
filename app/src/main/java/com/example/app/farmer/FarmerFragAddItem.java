2a36895856b0b5176a5d8cbaa3a0be978d8ff44e
Raman <Raman@DESKTOP-UUFUMDH>
2022-06-10 22:57:25 -0400

    Bottom Navigation with fragments for farmer side

---
+++ app/src/main/java/com/example/app/farmer/FarmerFragAddItem.java
@@ -0,0 +1,22 @@
+package com.example.app.farmer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.app.R;
+
+public class FarmerFragAddItem extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.farmerfragadditem, null);
+        getActivity().setTitle("Add Product");
+        return view;
+    }
+}
