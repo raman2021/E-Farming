83011c29b28ff142c79467508056d3e3725526dd
Raman <Raman@DESKTOP-UUFUMDH>
2022-06-10 22:55:14 -0400

    Bottom Navigation with fragments for farmer side

--- app/src/main/java/com/example/app/BottomNavigationFarmer.java
+++ app/src/main/java/com/example/app/BottomNavigationFarmer.java
@@ -1,14 +1,56 @@
 package com.example.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class BottomNavigationFarmer extends AppCompatActivity {
+import com.example.app.farmer.FarmerFrag;
+import com.example.app.farmer.FarmerFragAddItem;
+import com.example.app.farmer.FarmerFragOrders;
+import com.example.app.farmer.FarmerFragPending;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomNavigationFarmer extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation_farmer);
+        BottomNavigationView navigationView =  findViewById(R.id.farmernavigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.farmerHome:
+                fragment = new FarmerFrag();
+                break;
+            case R.id.Pending:
+                fragment = new FarmerFragPending();
+                break;
+            case R.id.Orders:
+                fragment = new FarmerFragOrders();
+                break;
+            case R.id.farmerProfile:
+                fragment = new FarmerFragAddItem();
+                break;
+        }
+        return loadfarmerrfragment(fragment);
+
+        //return false;
+    }
+
+    private boolean loadfarmerrfragment(Fragment fragment) {
+        if (fragment !=null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment,fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
