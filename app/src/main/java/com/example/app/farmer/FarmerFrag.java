ae2baef642fcf3db00d625af1683b7212a2b21c1
Raman <Raman@DESKTOP-UUFUMDH>
2022-06-13 16:47:27 -0400

    Logout for farmer

--- app/src/main/java/com/example/app/farmer/FarmerFrag.java
+++ app/src/main/java/com/example/app/farmer/FarmerFrag.java
@@ -1,7 +1,11 @@
 package com.example.app.farmer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,6 +14,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.app.R;
+import com.example.app.ShopPage;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FarmerFrag extends Fragment {
     @Nullable
@@ -17,6 +23,33 @@ public class FarmerFrag extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.farmerfrag, null);
         getActivity().setTitle("Home");
+        setHasOptionsMenu(true);
         return view;
     }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.logout,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int app = item.getItemId();
+        if (app == R.id.Logout) {
+            LogOut();
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void LogOut() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getActivity(), ShopPage.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
 }
+
